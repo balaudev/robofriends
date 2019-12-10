6be7577e9ef3226ba9b1c49a69833195846084de
baldedev <laurentbalebs@gmail.com>
2019-12-11 00:47:22 +0100

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-"projet React for fun"
-
-1.Clone this repo.
-2.Run npm install.
-3.Run npm start.
-
---
+++ precache-manifest.fb6167b8486f1e5e9b17311ec94b2eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "306d4c8ae5f45052514d",
+    "url": "/robofriends/static/css/main.deb9c67e.chunk.css"
+  },
+  {
+    "revision": "306d4c8ae5f45052514d",
+    "url": "/robofriends/static/js/main.306d4c8a.chunk.js"
+  },
+  {
+    "revision": "d5f315d43fa8a13d2e2c",
+    "url": "/robofriends/static/css/1.53e545fc.chunk.css"
+  },
+  {
+    "revision": "d5f315d43fa8a13d2e2c",
+    "url": "/robofriends/static/js/1.d5f315d4.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "33f254e511f2b31e28db3dc2829c45da",
+    "url": "/robofriends/index.html"
+  }
+];
